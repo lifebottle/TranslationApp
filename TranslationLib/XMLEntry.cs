1b7bbe09a9ab801d394ef06e9b5565fe068bf907
Stewie <fortiersteven1@gmail.com>
2022-10-08 18:12:39 -0400

    Fixing bug when saving XMLs
    Fix

--- TranslationLib/XMLEntry.cs
+++ TranslationLib/XMLEntry.cs
@@ -3,14 +3,19 @@ namespace TranslationLib
 {
     public class XMLEntry
     {
-        public int? Id { get; set; }
+        
         public string PointerOffset { get; set; }
+        public string VoiceId { get; set; }
         public string JapaneseText { get; set; }
         public string EnglishText { get; set; }
-        public string Status { get; set; }
         public string Notes { get; set; }
+        public int? Id { get; set; }
+        public int? StructId { get; set; }
         public int[] SpeakerId { get; set; }
         public int? UnknownPointer { get; set; }
+        public string Status { get; set; }
+        
+        
         [JsonIgnore] public string SpeakerName { get; set; }
     }
 }
