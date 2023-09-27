3c67cf84c2f3eb43b4079160284619da607e8d52
Mc-muffin <8714476+Mc-muffin@users.noreply.github.com>
2023-09-27 18:58:42 -0500

    Use vwf data for the preview
    Plus support for new lines and more characters

--- TranslationApp/fMain.cs
+++ TranslationApp/fMain.cs
@@ -23,6 +23,119 @@ namespace TranslationApp
         private string gameName;
         Bitmap fontAtlasImage;
 
+        struct font_glyph
+        {
+            public byte lskip;
+            public byte rskip;
+
+            public font_glyph(byte x, byte y)
+            {
+                lskip = x;
+                rskip = y;
+            }
+        }
+
+        private readonly font_glyph[] glyphs = new font_glyph[97]
+        {
+            /*    */ new font_glyph(10, 00),
+            /* ０ */ new font_glyph(05, 05),
+            /* １ */ new font_glyph(06, 05),
+            /* ２ */ new font_glyph(05, 05),
+            /* ３ */ new font_glyph(05, 06),
+            /* ４ */ new font_glyph(04, 07),
+            /* ５ */ new font_glyph(06, 06),
+            /* ６ */ new font_glyph(06, 06),
+            /* ７ */ new font_glyph(06, 07),
+            /* ８ */ new font_glyph(04, 05),
+            /* ９ */ new font_glyph(04, 04),
+            /* Ａ */ new font_glyph(04, 06),
+            /* Ｂ */ new font_glyph(05, 06),
+            /* Ｃ */ new font_glyph(05, 06),
+            /* Ｄ */ new font_glyph(05, 06),
+            /* Ｅ */ new font_glyph(05, 07),
+            /* Ｆ */ new font_glyph(05, 08),
+            /* Ｇ */ new font_glyph(05, 07),
+            /* Ｈ */ new font_glyph(05, 07),
+            /* Ｉ */ new font_glyph(08, 09),
+            /* Ｊ */ new font_glyph(07, 08),
+            /* Ｋ */ new font_glyph(05, 06),
+            /* Ｌ */ new font_glyph(05, 08),
+            /* Ｍ */ new font_glyph(05, 05),
+            /* Ｎ */ new font_glyph(05, 06),
+            /* Ｏ */ new font_glyph(05, 05),
+            /* Ｐ */ new font_glyph(05, 06),
+            /* Ｑ */ new font_glyph(05, 05),
+            /* Ｒ */ new font_glyph(05, 07),
+            /* Ｓ */ new font_glyph(06, 07),
+            /* Ｔ */ new font_glyph(05, 07),
+            /* Ｕ */ new font_glyph(05, 06),
+            /* Ｖ */ new font_glyph(05, 06),
+            /* Ｗ */ new font_glyph(02, 03),
+            /* Ｘ */ new font_glyph(05, 07),
+            /* Ｙ */ new font_glyph(05, 08),
+            /* Ｚ */ new font_glyph(05, 05),
+            /* ａ */ new font_glyph(06, 08),
+            /* ｂ */ new font_glyph(06, 07),
+            /* ｃ */ new font_glyph(07, 08),
+            /* ｄ */ new font_glyph(06, 07),
+            /* ｅ */ new font_glyph(06, 07),
+            /* ｆ */ new font_glyph(07, 09),
+            /* ｇ */ new font_glyph(06, 07),
+            /* ｈ */ new font_glyph(06, 07),
+            /* ｉ */ new font_glyph(08, 09),
+            /* ｊ */ new font_glyph(09, 10),
+            /* ｋ */ new font_glyph(05, 07),
+            /* ｌ */ new font_glyph(09, 09),
+            /* ｍ */ new font_glyph(03, 05),
+            /* ｎ */ new font_glyph(06, 07),
+            /* ｏ */ new font_glyph(06, 07),
+            /* ｐ */ new font_glyph(06, 07),
+            /* ｑ */ new font_glyph(06, 07),
+            /* ｒ */ new font_glyph(07, 09),
+            /* ｓ */ new font_glyph(07, 08),
+            /* ｔ */ new font_glyph(07, 08),
+            /* ｕ */ new font_glyph(06, 07),
+            /* ｖ */ new font_glyph(05, 07),
+            /* ｗ */ new font_glyph(03, 04),
+            /* ｘ */ new font_glyph(06, 08),
+            /* ｙ */ new font_glyph(05, 07),
+            /* ｚ */ new font_glyph(06, 07),
+            /* ， */ new font_glyph(01, 15),
+            /* ． */ new font_glyph(01, 15),
+            /* ・ */ new font_glyph(06, 08),
+            /* ： */ new font_glyph(08, 08),
+            /* ； */ new font_glyph(07, 08),
+            /* ？ */ new font_glyph(04, 05),
+            /* ！ */ new font_glyph(07, 09),
+            /* ／ */ new font_glyph(00, 01),
+            /* （ */ new font_glyph(12, 01),
+            /* ） */ new font_glyph(01, 13),
+            /* ［ */ new font_glyph(13, 01),
+            /* ］ */ new font_glyph(01, 11),
+            /* ｛ */ new font_glyph(14, 01),
+            /* ｝ */ new font_glyph(01, 14),
+            /* ＋ */ new font_glyph(03, 06),
+            /* － */ new font_glyph(06, 07),
+            /* ＝ */ new font_glyph(04, 03),
+            /* ＜ */ new font_glyph(06, 06),
+            /* ＞ */ new font_glyph(06, 06),
+            /* ％ */ new font_glyph(02, 09),
+            /* ＃ */ new font_glyph(04, 04),
+            /* ＆ */ new font_glyph(02, 04),
+            /* ＊ */ new font_glyph(04, 04),
+            /* ＠ */ new font_glyph(00, 01),
+            /* ｜ */ new font_glyph(08, 08),
+            /*  ” */ new font_glyph(01, 15),
+            /*  ’ */ new font_glyph(01, 18),
+            /* ＾ */ new font_glyph(07, 06),
+            /* 「 */ new font_glyph(10, 01),
+            /* 」 */ new font_glyph(01, 11),
+            /* 〜 */ new font_glyph(05, 06),
+            /* ＿ */ new font_glyph(00, 00),
+            /* 、 */ new font_glyph(00, 13),
+            /* 。 */ new font_glyph(01, 12),
+        };
+
         public fMain()
         {
             InitializeComponent();
@@ -914,21 +1027,31 @@ namespace TranslationApp
                 // handle null text
                 string text = (tbEnglishText.Text == null) ? "" : tbEnglishText.Text;
 
-                foreach (char character in text)
+                foreach (char c in text)
                 {
-                    Rectangle characterRectangleFromAtlas = GetCharacterRectangleFromAtlas(character);
-                    RectangleF destRect = new RectangleF(location, new SizeF(characterRectangleFromAtlas.Width, characterRectangleFromAtlas.Height));
-                    graphics.DrawImage(fontAtlasImage, destRect, characterRectangleFromAtlas, GraphicsUnit.Pixel);
-                    location.X += characterRectangleFromAtlas.Width;
+                    Rectangle charRect = GetCharacterRectangleFromAtlas(c, out int shift, out bool line);
+                    RectangleF destRect = new RectangleF(location, new SizeF(charRect.Width, charRect.Height));
+                    graphics.DrawImage(fontAtlasImage, destRect, charRect, GraphicsUnit.Pixel);
+                    
+                    if (line)
+                    {
+                        location.X = 0f;
+                        location.Y += 24f;
+                    }
+                    else
+                    {
+                        location.X += charRect.Width - shift;
+                    }
                 }
                 graphics.ResetTransform();
             }
         }
 
-        private Rectangle GetCharacterRectangleFromAtlas(int character)
+        private Rectangle GetCharacterRectangleFromAtlas(int character, out int s, out bool addline)
         {
             int charWidth = 24;
             int charHeight = 24;
+            addline = false;
 
             // Calculate the index of the character in the font atlas
             int index;
@@ -944,16 +1067,94 @@ namespace TranslationApp
             {
                 index = character - 0x3C;
             }
-            else
+            else switch (character)
             {
-                index = 0;
+                case '\n':
+                    index = 0;
+                    addline = true;
+                    break;
+                case '!':
+                    index = 69;
+                    break;
+                case ',':
+                    index = 63;
+                    break;
+                case '/':
+                    index = 70;
+                    break;
+                case '~':
+                    index = 93;
+                    break;
+                case '_':
+                    index = 94;
+                    break;
+                case '+':
+                    index = 77;
+                    break;
+                case '*':
+                    index = 85;
+                    break;
+                case '=':
+                    index = 79;
+                    break;
+                case '(':
+                    index = 71;
+                    break;
+                case ')':
+                    index = 72;
+                    break;
+                case '[':
+                    index = 73;
+                    break;
+                case ']':
+                    index = 74;
+                    break;
+                case '{':
+                    index = 75;
+                    break;
+                case '}':
+                    index = 76;
+                    break;
+                case '-':
+                    index = 78;
+                    break;
+                case '\'':
+                    index = 89;
+                    break;
+                case '"':
+                    index = 88;
+                    break;
+                case '.':
+                    index = 64;
+                    break;
+                case ':':
+                    index = 66;
+                    break;
+                case ';':
+                    index = 67;
+                    break;
+                case '?':
+                    index = 68;
+                    break;
+                case '<':
+                    index = 80;
+                    break;
+                case '>':
+                    index = 81;
+                    break;
+                default:
+                    index = 0;
+                    break;
             }
 
             // Calculate the position of the character in the atlas based on its index
             int y = index * charHeight;
-            int x = 0;
+            int x = glyphs[index].lskip;
 
+            charWidth -= glyphs[index].lskip;
+            s = glyphs[index].rskip;
             return new Rectangle(x, y, charWidth, charHeight);
+
         }
     }
 }
